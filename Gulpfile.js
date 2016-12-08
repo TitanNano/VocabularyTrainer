1f3f71f654bc5d90a69f9b358dce5ff92c2adcb9
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2016-12-08 12:10:13 +0100

    update build process
    - use webpack instead of SystemJS
    - copy new static files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,7 @@
-var Gulp = require('gulp');
-var Babel = require('gulp-babel');
-var Sourcemaps = require('gulp-sourcemaps');
-var Webpack = require("webpack");
-var path = require('path');
-var Gutil = require('gulp-util');
+let Gulp = require('gulp');
+let Webpack = require("webpack");
+let path = require('path');
+let Gutil = require('gulp-util');
 
 Gulp.task("webpack", function(callback) {
     // run webpack
@@ -27,7 +25,7 @@ Gulp.task("webpack", function(callback) {
         },
         devtool : '#source-map',
     }, function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
+        if(err) throw new Gutil.PluginError("webpack", err);
         Gutil.log("[webpack]", stats.toString());
         callback();
     });
@@ -38,24 +36,14 @@ Gulp.task('copyBower', function(){
         .pipe(Gulp.dest('dist/components/'));
 });
 
-Gulp.task('copy:js', function(){
-    return Gulp.src('src/**/*.js')
-        .pipe(Gulp.dest('temp/'));
-});
-
 Gulp.task('copy:no-compile', function(){
-    return Gulp.src(['src/**/*.html', 'src/**/*.css'])
+    return Gulp.src(['src/**/*.html', 'src/**/*.css', 'src/**/*.json', 'src/**/*.webmanifest', 'src/**/*.png', '!src/af/**'])
         .pipe(Gulp.dest('dist/'));
 });
 
-Gulp.task('compile', ['copy:js'], function(){
-    return Gulp.src('temp/**/*.js')
-        .pipe(Sourcemaps.init())
-        .pipe(Babel({
-            presets: ['es2015']
-        }))
-        .pipe(Sourcemaps.write('.'))
-        .pipe(Gulp.dest('temp/'));
-});
+Gulp.task('compile', ['webpack', 'copyBower', 'copy:no-compile'], function() {
+    return Gulp.src('dist/components/**/*.js')
+        .pipe(Gulp.dest('dist/components/'));
+})
 
-Gulp.task('default', ['copyBower', 'copy:no-compile', 'webpack']);
+Gulp.task('default', ['compile']);
